7320cee8132c068793ab2eca9cb09f1c664cc60a
Linus Baumann <inf20001@lehre.dhbw-stuttgart.de>
2022-12-10 23:24:49 +0100

    test enhanced

--- app/app/tests/functional/test_index.py
+++ app/app/tests/functional/test_index.py
@@ -1,15 +1,29 @@
-def test_index(client):
+def test_index_page_loaded(client):
     response = client.get("/", follow_redirects=True)
-    assert response.status_code == 200
+    assert response.status_code is 200
     assert "Hello DHBW" in response.get_data(as_text=True)
 
-def test_hello(client):
+def test_hello_page_loaded_with_name(client):
     response = client.post(
         "/hello",
         data = dict(name="Bernd"),
         follow_redirects = True
     )
-    assert response.status_code == 200
+    assert response.status_code is 200
     assert "Hello Bernd" in response.get_data(as_text=True)
-    #assert response.imageLoaded
+
+def test_hello_page_loaded_without_name(client):
+    response = client.post(
+        "/hello",
+        follow_redirects = True
+    )
+    assert response.status_code is 200
+    assert response.request.path == "/hello"
+    assert "Hello DHBW" in response.get_data(as_text=True)
+
+def test_favicon_loaded(client):
+    response = client.get("/favicon.ico")
+    assert response.status_code is 200
+    assert response.content_type == "image/vnd.microsoft.icon"
+
 
